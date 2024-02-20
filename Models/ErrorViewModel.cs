d4ddd6b6d6921ef3091d2e0d344d5fed5cf01160
mpr00 <mpr00@DESKTOP-M1F2NO3>
2024-02-20 08:45:01 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestingGitHub_Podium.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
